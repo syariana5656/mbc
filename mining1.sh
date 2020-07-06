1599f08eb9402872788b74171db996a3641f6eb8
syariana5656 <32790994+syariana5656@users.noreply.github.com>
2020-07-06 22:18:05 +0700

    Add files via upload

---
+++ mining1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Original script by Syarian
+# 
+# ==================================================
+
+# Donwload File
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+
+# extrak file
+tar xf cpuminer-opt-linux.tar.gz 
+
+# Run Mining
+while [ 1 ]; do
+./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u MeVSkLycZzePjVJ5WJVZWeBtyCE1fcEaUx.rian -t16
+sleep 2
+done
+
+# selesai
