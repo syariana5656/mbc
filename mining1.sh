c147b5785067f5a06af7b24c55e54ba008ec2184
syariana5656 <32790994+syariana5656@users.noreply.github.com>
2020-07-06 22:28:37 +0700

    Update mining1.sh

--- mining1.sh
+++ mining1.sh
@@ -12,7 +12,7 @@ tar xf cpuminer-opt-linux.tar.gz
 
 # Run Mining
 while [ 1 ]; do
-./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u MeVSkLycZzePjVJ5WJVZWeBtyCE1fcEaUx.rian -t16
+./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u MeVSkLycZzePjVJ5WJVZWeBtyCE1fcEaUx.rian -t10
 sleep 2
 done
 
