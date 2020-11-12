57e47ed592fcdfbb4c18b303e4cedd746c847de3
syariana5656 <32790994+syariana5656@users.noreply.github.com>
2020-11-12 16:24:10 +0700

    Update mining.sh

--- mining.sh
+++ mining.sh
@@ -10,8 +10,6 @@
 # ==================================================
 
 # SUGAR EU
-screen -R aku
-
 # Donwload File
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 
