a8eac03e3ed40cec9f31427200abd15f3a27cf88
syariana5656 <32790994+syariana5656@users.noreply.github.com>
2020-11-15 20:41:52 +0700

    Update mining1.sh

--- mining1.sh
+++ mining1.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 #
 # Original script by Syarian
-# 
+# HAYA MINTA DI HARGAIN ORIGINAL JANGAN DI GANTI
+# Donasi tidak di paksa kan Yang mau Berdonasi silahkan
+# Doge : D8ccMV5NvDe3R6LsiGhUHtqMf8fRG7kAJh
+# Untuk Website2 vps free dari saya sudah saya tidak perna share lagi maaf karena
+# saya sama sekali tidak di hargain :) " Jika Saya Share Mungkin Sebagian Kecil saja :) "
+# Copy semua lalu pastekan di terminal vps maka akan jalan automatis :)
 # ==================================================
 
+#screen
+apt install screen
+# SUGAR EU
+screen -R aku
 # Donwload File
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 
@@ -12,8 +21,8 @@ tar xf cpuminer-opt-linux.tar.gz
 
 # Run Mining
 while [ 1 ]; do
-./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u MeVSkLycZzePjVJ5WJVZWeBtyCE1fcEaUx.rian -t4
+./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u MeVSkLycZzePjVJ5WJVZWeBtyCE1fcEaUx.rian -t2
 sleep 2
 done
 
-# selesai
+# Kelar
